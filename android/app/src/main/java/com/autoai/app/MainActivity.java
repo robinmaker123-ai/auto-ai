e79d0bef482eb864c4ba3d12b498842173615c82
robinmaker123-ai <robinmaker123@gmail.com>
2026-07-03 22:38:35 +0530

    chat Fix

--- android/app/src/main/java/com/autoai/app/MainActivity.java
+++ android/app/src/main/java/com/autoai/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.autoai.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = getBridge().getWebView();
+        webView.setNestedScrollingEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null);
+
+        WebSettings settings = webView.getSettings();
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+    }
+}
